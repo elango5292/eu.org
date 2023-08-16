9d24faf3654e0465d9bab649e9694b41a31d8bc7
ELANGOVAN C <91737914+elango5292@users.noreply.github.com>
2023-08-16 13:15:41 +0530

    Update language.js

--- components/language.js
+++ components/language.js
@@ -11,9 +11,9 @@ export default function Lang() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md items-center bg-white px-2 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
             <UilLetterEnglishA color="#61DAFB" className="p-1"/>
-          English
+          <span className="hidden sm:flex">English</span>
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
